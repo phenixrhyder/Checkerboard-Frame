8ce062947398d23b02382bd24db80d556a6e6f9a
phenixrhyder <161587348+phenixrhyder@users.noreply.github.com>
2025-08-13 23:16:45 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 # app.py
 from flask import Flask, send_from_directory
 
-# This creates the web server.
 app = Flask(__name__)
 
-# This tells the server to look for your main HTML file.
 @app.route('/')
 def home():
     return send_from_directory('.', 'index.html')
 
-# This runs the server.
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=7860)
